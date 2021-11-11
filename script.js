a7c507cbe5ebc2efc5486e46a3ea2241709d0bad
duorfan <nf2111@nyu.edu>
2021-11-11 12:35:56 -0500

    example

---
+++ script.js
@@ -0,0 +1,39 @@
+const app = {
+
+    data: detailedBandObject, //this comes from my data.js files
+  
+    initialize: () => {
+      $('.box').click(e => {
+        $(e.currentTarget).toggleClass('transform')  
+      });
+  
+      $('.circle').dblclick(e => {
+        $(e.currentTarget).toggleClass('moveMe')      
+      });
+  
+      $('.listBeatles').click(function () {
+        app.listBeatles();
+      })
+  
+      $('.compareBands').click(function () {
+        app.compareBands();
+      })
+    },
+  
+    listBeatles: () => {
+      // in this instance can also use this.data, it means the same thing. app.data is safer
+      app.data.beatles.albums.forEach(album => {
+        debugger;
+        $('.beatlesAlbums').append(`<li>${album.name}, ${album.year}</li>`);
+      });
+    },
+  
+    compareBands: () => {
+      const nirvanaMembers = app.data.nirvana.members;
+      const beatlesMembers = app.data.beatles.members;
+  
+      $('.bandsComparison').text(
+        `${nirvanaMembers.length > beatlesMembers.length ? 'Nirvana' : 'Beatles'} 
+      had more members`);
+    }
+  }
